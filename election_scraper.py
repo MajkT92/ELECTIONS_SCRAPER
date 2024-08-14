bbfbce638df44ffb9382a4dc5faad98b82f3a5d8
mtoman92 <mtoman92@gmail.com>
2024-08-14 11:43:10 +0200

    Import knihoven

---
+++ election_scraper.py
@@ -0,0 +1,12 @@
+"""
+election_scraper.py: třetí projekt do Engeto Online Python Akademie
+author: Michal Toman
+email: mtoman92@gmail.com
+discord: majk923278
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+import argparse
+
